faa220e567f4877797cf0e07fae116d1288c8f43
zhou <1357530094@qq.com>
2020-05-26 10:14:36 +0800

    +)首页；+）新闻页

---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="content">
+		<van-button type="default">默认按钮</van-button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
